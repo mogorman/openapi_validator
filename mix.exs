2434dd4614ec627b9f5d1607a754e57dd6008606
Matthew O'Gorman <mog@rldn.net>
2019-10-02 00:03:28 -0400

    working on making it publishable in hex

--- mix.exs
+++ mix.exs
@@ -8,6 +8,9 @@ defmodule OpenapiValidator.MixProject do
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      description: description(),
+      package: package(),
+      source_url: "https://github.com/mogorman84/openapi_validator",
     ]
   end
 
@@ -18,14 +21,27 @@ defmodule OpenapiValidator.MixProject do
     ]
   end
 
+  defp description() do
+    "Tools for your phoenix project for validating openapi spec in your application."
+  end
+
+  defp package() do
+    [
+      maintainers: ["Matthew O'Gorman"],
+      links: %{"GitHub" => "https://github.com/mogorman84/openapi_validator"},
+      licenses: ["MIT"],
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:jason, "~> 1.1", override: true},
+      {:jason, "~> 1.1"},
       {:plug, ">= 1.6.0"},
       {:ex_json_schema, "~> 0.6.1"},
       {:ex_spec, "~> 2.0.0", only: :test},
       {:freedom_formatter, "~> 1.1", only: [:dev, :test]},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
     ]
   end
 end
